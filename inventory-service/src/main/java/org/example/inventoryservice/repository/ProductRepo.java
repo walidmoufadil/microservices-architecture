a14d338cc9d78e1ff4877b197cc5754e28f4ab4a
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-16 19:18:23 +0100

    add repository package

--- inventory-service/src/main/java/org/example/inventoryservice/repository/ProductRepo.java
+++ inventory-service/src/main/java/org/example/inventoryservice/repository/ProductRepo.java
@@ -1,4 +1,9 @@
 package org.example.inventoryservice.repository;
 
-public interface ProductRepo {
+import org.example.inventoryservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepo extends JpaRepository<Product,String> {
 }
