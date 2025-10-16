7655c0d570b3d3e076bac339cd8c4191839410a8
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-16 19:00:12 +0100

    add test data with a Bean

--- customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,26 @@
 package org.example.customerservice;
 
+import org.example.customerservice.entity.Customer;
+import org.example.customerservice.repository.CustomerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(CustomerRepo customerRepo) {
+        return args -> {
+            customerRepo.save(Customer.builder().name("John Doe").email("John@gmail.com").build());
+            customerRepo.save(Customer.builder().name("Jane Smith").email("Jane@gmail.com").build());
+            customerRepo.save(Customer.builder().name("Alice Johnson").email("Alice@gmail.com").build());
+        };
     }
 
 }
