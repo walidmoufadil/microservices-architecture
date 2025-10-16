9d1bc05b4a97ff5293c15bd6014945cc4313d67e
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-16 18:59:42 +0100

    add repository package

--- customer-service/src/main/java/org/example/customerservice/repository/CustomerRepo.java
+++ customer-service/src/main/java/org/example/customerservice/repository/CustomerRepo.java
@@ -1,4 +1,11 @@
 package org.example.customerservice.repository;
 
-public interface CustomerRepo {
+import org.example.customerservice.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 }
