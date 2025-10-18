406e16e7e9f42f260c1410295c524cc5305e2740
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:15:26 +0100

    add repository

--- billing-service/src/main/java/org/example/billingservice/repository/BillRepo.java
+++ billing-service/src/main/java/org/example/billingservice/repository/BillRepo.java
@@ -1,4 +1,9 @@
 package org.example.billingservice.repository;
 
-public interface BillRepo {
+import org.example.billingservice.entity.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BillRepo extends JpaRepository<Bill,Long> {
 }
--- billing-service/src/main/java/org/example/billingservice/repository/ProductItemRepo.java
+++ billing-service/src/main/java/org/example/billingservice/repository/ProductItemRepo.java
@@ -1,4 +1,9 @@
 package org.example.billingservice.repository;
 
-public interface ProductItemRepo {
+import org.example.billingservice.entity.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductItemRepo extends JpaRepository<ProductItem, Long> {
 }
