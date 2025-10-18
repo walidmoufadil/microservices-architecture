c9a223985cec5a3a6ce3724c4c2418b0644aff6f
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:16:31 +0100

    add web package

--- billing-service/src/main/java/org/example/billingservice/web/BillingRestController.java
+++ billing-service/src/main/java/org/example/billingservice/web/BillingRestController.java
@@ -1,4 +1,38 @@
 package org.example.billingservice.web;
 
+import lombok.RequiredArgsConstructor;
+import org.example.billingservice.entity.Bill;
+import org.example.billingservice.feign.CustomerClientRest;
+import org.example.billingservice.feign.ProductClientRest;
+import org.example.billingservice.repository.BillRepo;
+import org.example.billingservice.repository.ProductItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class BillingRestController {
+    @Autowired
+    private BillRepo billRepo;
+    @Autowired
+    private ProductItemRepo productItemRepo;
+    @Autowired
+    private CustomerClientRest customerClientRest;
+    @Autowired
+    private ProductClientRest productClientRest;
+
+    @GetMapping("bills/{id}")
+    public Bill getBillById(@PathVariable Long id){
+        Bill bill = billRepo.findById(id).orElseThrow(()->new RuntimeException("Bill not found"));
+        bill.setCustomer(customerClientRest.getCustomerById(bill.getCustomerId()));
+        bill.getProductItems().forEach(productItem -> {
+            productItem.setProduct(productClientRest.getProductById(productItem.getProductId()));
+        });
+        return bill;
+    }
 }
