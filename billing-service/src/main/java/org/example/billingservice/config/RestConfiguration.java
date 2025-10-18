da0d1ed03ecc1b17c58cf6a688b81ea7ca52a7f5
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:15:54 +0100

    add config

--- billing-service/src/main/java/org/example/billingservice/config/RestConfiguration.java
+++ billing-service/src/main/java/org/example/billingservice/config/RestConfiguration.java
@@ -1,4 +1,16 @@
 package org.example.billingservice.config;
 
-public class RestConfiguration {
+import org.example.billingservice.entity.Bill;
+import org.example.billingservice.entity.ProductItem;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(ProductItem.class, Bill.class);
+    }
 }
