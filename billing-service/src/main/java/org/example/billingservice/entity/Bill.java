7c2819346ea0bd4121d79fdec50c0d08fb4d3d36
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:15:05 +0100

    add entity

--- billing-service/src/main/java/org/example/billingservice/entity/Bill.java
+++ billing-service/src/main/java/org/example/billingservice/entity/Bill.java
@@ -1,4 +1,26 @@
 package org.example.billingservice.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.billingservice.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Bill {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billDate;
+    private Long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems;
+    @Transient
+    private Customer customer;
 }
--- billing-service/src/main/java/org/example/billingservice/entity/ProductItem.java
+++ billing-service/src/main/java/org/example/billingservice/entity/ProductItem.java
@@ -1,4 +1,28 @@
 package org.example.billingservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.billingservice.model.Product;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductItem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String productId;
+    private int quantity;
+    private double unitPrice;
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Bill bill;
+    @Transient
+    private Product product;
+
 }
