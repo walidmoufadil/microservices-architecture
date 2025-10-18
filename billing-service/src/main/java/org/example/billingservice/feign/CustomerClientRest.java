6eb5f8c83c2b920c24862dac3fbb1c83ca7a8e43
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:16:11 +0100

    add open fein client

--- billing-service/src/main/java/org/example/billingservice/feign/CustomerClientRest.java
+++ billing-service/src/main/java/org/example/billingservice/feign/CustomerClientRest.java
@@ -1,4 +1,17 @@
 package org.example.billingservice.feign;
 
+import org.example.billingservice.model.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "customer-service")
 public interface CustomerClientRest {
+    @GetMapping("/customers/{id}")
+    Customer getCustomerById(@PathVariable Long id);
+
+    @GetMapping("/customers")
+    PagedModel<Customer> getAllCustomers();
 }
--- billing-service/src/main/java/org/example/billingservice/feign/ProductClientRest.java
+++ billing-service/src/main/java/org/example/billingservice/feign/ProductClientRest.java
@@ -1,4 +1,16 @@
 package org.example.billingservice.feign;
 
+import org.example.billingservice.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "inventory-service")
 public interface ProductClientRest {
+    @GetMapping("/products/{id}")
+    Product getProductById(@PathVariable String id);
+
+    @GetMapping("/products")
+    PagedModel<Product> getAllProducts();
 }
