e5d5929e4f3def32ab612d065966667154cea197
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-18 15:15:38 +0100

    add model

--- billing-service/src/main/java/org/example/billingservice/model/Customer.java
+++ billing-service/src/main/java/org/example/billingservice/model/Customer.java
@@ -1,4 +1,10 @@
 package org.example.billingservice.model;
 
+import lombok.Data;
+
+@Data
 public class Customer {
+    private Long id;
+    private String name;
+    private String email;
 }
--- billing-service/src/main/java/org/example/billingservice/model/Product.java
+++ billing-service/src/main/java/org/example/billingservice/model/Product.java
@@ -1,4 +1,11 @@
 package org.example.billingservice.model;
 
+import lombok.Data;
+
+@Data
 public class Product {
+    private String id;
+    private String name;
+    private int quantity;
+    private double price;
 }
